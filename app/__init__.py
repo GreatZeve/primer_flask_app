d508b79b818daaebdb5a8f734f48973556cff503
César Ángeles <ces2407113@hybridge.education>
2025-06-04 17:00:18 -0600

    Correcciones al código

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import config
 
 def create_app():
-app=Flask(--name__)
-app.config.from_object(config.Confing)
-
-from .views import main as main_blueprint
-app.register_blueprint(main_blueprint)
-
-return app
+    app = Flask(__name__)
+    app.config.from_object(config.Config)
+    
+    from .views import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    return app
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import create_app
 
-app=create_app()
+app = create_app()
 
-if__name__==__main__:
-app.run(debug=True, port=5050)
+if __name__ == '__main__':
+    app.run(debug=True, port=5050)
